6b4be763098117b91f3ad8eb040a30789a46370d
GabrielRosa <gabrielflororosa2007@gmail.cim>
2025-07-29 19:55:14 -0300

    Add project files.

---
+++ ToDoAPI/Controllers/TarefaController.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ToDoAPI.Entities;
+using ToDoAPI.Repositories;
+
+namespace ToDoAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TarefasController : ControllerBase
+    {
+        private readonly TarefaRepository _repo;
+
+        public TarefasController(TarefaRepository repo)
+        {
+            _repo = repo;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Get() =>
+            Ok(await _repo.ListarAsync());
+
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] Tarefas tarefa)
+        {
+            await _repo.AdicionarAsync(tarefa);
+            return Created("", tarefa);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] Tarefas tarefa)
+        {
+            tarefa.Id = id;
+            await _repo.AtualizarAsync(tarefa);
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _repo.ExcluirAsync(id);
+            return NoContent();
+        }
+    }
+}
---
+++ ToDoAPI/Repositories/TarefaRepository.cs
@@ -0,0 +1,72 @@
+﻿using Dapper;
+using System.Data;
+using ToDoAPI.Context;
+using ToDoAPI.Entities;
+
+namespace ToDoAPI.Repositories
+{
+    public class TarefaRepository
+    {
+        private readonly DapperContext _context;
+
+        public TarefaRepository(DapperContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<Tarefas>> ListarAsync()
+        {
+            using var connection = _context.CreateConnection();
+            var result = await connection.QueryAsync<Tarefas>(
+                "gabrieltodo.sp_ListarTarefas",
+                commandType: CommandType.StoredProcedure
+            );
+            return result.ToList();
+        }
+
+        public async Task AdicionarAsync(Tarefas tarefa)
+        {
+            using var connection = _context.CreateConnection();
+            await connection.ExecuteAsync(
+                "gabrieltodo.sp_AdicionarTarefa",
+                new
+                {
+                    tarefa.Titulo,
+                    tarefa.Descricao,
+                    tarefa.Concluida,
+                    tarefa.DataCriacao,
+                    tarefa.DataConclusao
+                },
+                commandType: CommandType.StoredProcedure
+            );
+        }
+
+        public async Task ExcluirAsync(int id)
+        {
+            using var connection = _context.CreateConnection();
+            await connection.ExecuteAsync(
+                "gabrieltodo.sp_ExcluirTarefa",
+                new { p_id = id },
+                commandType: CommandType.StoredProcedure
+            );
+        }
+
+        public async Task AtualizarAsync(Tarefas tarefa)
+        {
+            using var connection = _context.CreateConnection();
+            await connection.ExecuteAsync(
+                "gabrieltodo.sp_AtualizarTarefa",
+                new
+                {
+                    tarefa.Id,
+                    tarefa.Titulo,
+                    tarefa.Descricao,
+                    tarefa.Concluida,
+                    tarefa.DataCriacao,
+                    tarefa.DataConclusao
+                },
+                commandType: CommandType.StoredProcedure
+            );
+        }
+    }
+}
